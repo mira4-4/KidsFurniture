34e453ea7100c73f15b35dc4e2cab8b6003be735
mira4-4 <m.ivanova2021@pmg-pernik.com>
2026-02-23 13:46:19 +0200

    4chast 6str

---
+++ KidsFurniture/Models/Brand/BrandPairVM.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KidsFurniture.Models.Brand
+{
+    public class BrandPairVM
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Brand")]
+        public string Name { get; set; } = null!;
+    }
+}
---
+++ KidsFurniture/Models/Category/CategoryPairVM.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KidsFurniture.Models.Category
+{
+    public class CategoryPairVM
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Category")]
+        public string Name { get; set; } = null!;
+    }
+
+}
