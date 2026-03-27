db664fc44fa050416949fc3144775bd2d5b79271
mira4-4 <m.ivanova2021@pmg-pernik.com>
2026-03-27 15:11:44 +0200

    part 5

---
+++ KidsFurnitureApp.Core/Contracts/IOrderService.cs
@@ -0,0 +1,27 @@
+﻿using KidsFurniture.Infrastructure.Data;
+using KidsFurniture.Infrastructure.Data.Entities;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KidsFurnitureApp.Core.Contracts
+{
+    public interface IOrderService
+    {
+        bool Create(int productId, string userId, int quantity);
+
+        List<Order> GetOrders();
+
+        List<Order> GetOrdersByUser(string userId);
+
+        Order GetOrderById(int orderId);
+
+        bool RemoveById(int orderId);
+
+        bool Update(int orderId, int productId, string userId, int quantity);
+    }
+
+}
